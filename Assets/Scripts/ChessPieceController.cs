22bdd1f7d72ca136e5f35aeb2664f3836b9b7e00
julialwang <julialwa@cs.washington.edu>
2023-05-27 00:08:15 -0700

    untested piece snapping to grid, new scene for title page with UI

--- Assets/Scripts/ChessPieceController.cs
+++ Assets/Scripts/ChessPieceController.cs
@@ -1,21 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.XR;
 
 public class ChessPieceController : MonoBehaviour
 {
     public Rigidbody rb;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float gridSize = 3.0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.transform.position = new Vector3(
+            RoundToNearestGrid(rb.transform.position.x),
+            RoundToNearestGrid(rb.transform.position.y),
+            RoundToNearestGrid(rb.transform.position.z));
+    }
+
+    private float RoundToNearestGrid(float pos)
+    {
+        float xDiff = pos % gridSize;
+        pos -= xDiff;
+        if (xDiff > (gridSize / 2))
+        {
+            pos += gridSize;
+        }
+        return pos;
     }
 }
